b48f1b908e71b8eb2ea12d82cc74caf9ecb6c59d
ackrad <egcorak@gmail.com>
2021-05-25 23:36:05 +0300

    Added Save and load

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameController : MonoBehaviour
 {
@@ -26,17 +29,20 @@ public class GameController : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(this.gameObject);
+        Load();
+
     }
     void Start()
     {
-        
+
+  
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-   
+        
 
     }
 
@@ -70,10 +76,52 @@ public class GameController : MonoBehaviour
     {
         IncreaseCoins(coinAmountToIncrease);
 
+        Save();
         yield return new WaitForSeconds(secondsToWait);
 
         LoadNextScene();
 
     }
 
+
+
+    private void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerCoinData data = new PlayerCoinData
+        {
+            coinAmount = coinAmount
+        };
+
+        bf.Serialize(file, data);
+
+        file.Close();
+
+    }
+
+    private void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath+"/playerInfo.dat", FileMode.Open);
+            PlayerCoinData data = (PlayerCoinData) bf.Deserialize(file);
+            file.Close();
+
+            coinAmount = data.coinAmount;
+            IncreaseCoins(0); // burayý daha düzgün yaz sonra stringi update etmek için kullanýyorsun
+        }
+
+
+
+    }
+
+}
+
+
+[Serializable]
+class PlayerCoinData
+{
+    public int coinAmount;
 }
